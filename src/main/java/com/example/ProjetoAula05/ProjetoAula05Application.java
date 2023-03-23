51bcef7d8e1e91edab24a66291d64d0220b2ebe9
thi-costa <41833533+thi-costa@users.noreply.github.com>
2023-03-22 22:28:45 -0300

    chore: application and applicationTests

---
+++ src/main/java/com/example/ProjetoAula05/ProjetoAula05Application.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoAula05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAula05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAula05Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjetoAula05/ProjetoAula05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoAula05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAula05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
